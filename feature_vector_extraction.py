d8cddc4c1be4c21dff93e61beb55319ee31b0126
Alexandros Metsai <alexmetsai@gmail.com>
2019-06-05 10:03:14 +0300

    fixed license notice

--- feature_vector_extraction.py
+++ feature_vector_extraction.py
@@ -1,23 +1,25 @@
-# Extract feature vectors from video frames.
-# These features come from the Pool5 layers of a ResNet deep
-# neural network, pre-trained on ImageNet. The algorithm captures
-# frames directly from video, there is not need for prior frame extraction.
+'''
+Extract feature vectors from video frames.
+These features come from the Pool5 layers of a ResNet deep
+neural network, pre-trained on ImageNet. The algorithm captures
+frames directly from video, there is not need for prior frame extraction.
 
-# Copyright (C) 2019 Alexandros I. Metsai
-# alexmetsai@gmail.com
+Copyright (C) 2019 Alexandros I. Metsai
+alexmetsai@gmail.com
 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 import os
 import torch
